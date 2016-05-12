43fd07236f630a9fcf43a6193f4d0cd6b81a4ffd
Saroj Sharma <saroj.webdeveloper@gmail.com>
2016-05-13 01:22:21 +0530

    Modified on create action for unvalidated information

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,12 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-    redirect_to article_path(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
 
   private
