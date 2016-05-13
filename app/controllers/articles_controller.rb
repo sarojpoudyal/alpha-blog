7b6883599c0c87c49f5d262db299f9b88ffe3d55
Saroj Sharma <saroj.webdeveloper@gmail.com>
2016-05-13 23:20:53 +0530

    Redudancy removed from article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only:[:edit, :update, :show, :destroy]
   def new
     @article = Article.new
   end
@@ -14,15 +15,15 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def update
-    @article = Article.find(params[:id])
+    
 
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
@@ -37,7 +38,7 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
@@ -45,5 +46,8 @@ class ArticlesController < ApplicationController
   private
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
